c5dcf5269d7fd65f60870963a02dbb7b0e815ab4
Rustam Uzairov <mr.uzairov@gmail.com>
2023-08-03 21:16:48 +0300

    good

--- test.sh
+++ test.sh
@@ -1,57 +1,74 @@
 #!/bin/bash
 
+if [ -f "$HOME"/keysnet ]; then
+	source "$HOME"/keysnet
+else
+	echo "keys file file not found"
+fi
+
 renderer_n=networkd
 renderer_N=NetworkManager
 lan_name=enp2s0
-dhcp4_tru=true
-dhcp4_false=no
-dhcp4_addresses1=[192.168.1.27/24]
-dhcp4_addresses2=[192.168.0.27/24]
-routes_via1=192.168.1.1
-routes_via2=192.168.0.1
-nameserv_addr_def=[8.8.8.8,8.8.4.4]
-nameserv_addr1=[192.168.1.1,8.8.8.8]
-nameserv_addr2=[192.168.0.1,8.8.8.8]
+wan_name=wlx60e32716669c
 
-source "$HOME"/keysnet
+name_point=2 #1,2,3
+# var_routes=  #0,1
+dhcp4=1 #1=true/2=no
 
-# network:
-#   version: 2
-#   renderer: $renderer_N
-#   wifis:
-#     $wan_name:
-#       access-points:
-#         $wan_point2:
-#           password: $wan_pass_point2
-#       # dhcp4: $dhcp4_false
-#
-#       dhcp4: $dhcp4_false
-#       addresses: $dhcp4_addresses2
-#       routes:
-#       - to: default
-#         via: $routes_via2
-#       nameservers:
-#         addresses: $nameserv_addr2
-#
-
-echo_f {
-echo "network:                               "     
-echo "  version: 2                           "      
-echo "  renderer: $renderer_N                "     
-echo "  wifis:                               "   
-echo "    $wan_name:                         "   
-echo "      access-points:                   "   
-echo "        $wan_point2:                   "   
-echo "          password: $wan_pass_point2   "   
-echo "                                       "       
-echo "      dhcp4: $dhcp4_false              "         
-echo "      addresses: $dhcp4_addresses2     "     
-echo "      routes:                          "           
-echo "      - to: default                    "           
-echo "        via: $routes_via2              "         
-echo "      nameservers:                     "         
-echo "        addresses: $nameserv_addr2     "           
+echo_f() {
+	echo "network:                               "
+	echo "  version: 2                           "
+	echo "  renderer: $renderer_N                "
+	echo "  wifis:                               "
+	echo "    $wan_name:                         "
+	echo "      access-points:                   "
+	echo "        $wan_pt:                       "
+	echo "          password: $wan_pass          "
+}
+dhcp4_f() {
+	echo "      dhcp4: $dhcp4_ref                "
 }
 
-echo_f
+dhcp4_stat() {
+	echo "      addresses: $dhcp4_addresses      "
+	echo "      routes:                          "
+	echo "      - to: default                    "
+	echo "        via: $routes_via               "
+	echo "      nameservers:                     "
+	echo "        addresses: $nameserv_addr      "
+}
 
+if [ $name_point == 1 ]; then
+	wan_pt=$wan_point1
+	wan_pass=$wan_pass_point1
+	var_routes=1
+fi
+
+if [ $name_point == 2 ]; then
+	wan_pt=$wan_point2
+	wan_pass=$wan_pass_point2
+	var_routes=0
+	# wan_pass=wan_pass_point$name_point
+fi
+
+if [ $name_point == 3 ]; then
+	wan_pt=$wan_point2
+	wan_pass=$wan_pass_point3
+	# var_routes=0
+fi
+
+dhcp4_addresses=[192.168.$var_routes.27/24]
+routes_via=192.168.$var_routes.1
+nameserv_addr_def=[8.8.8.8,8.8.4.4]
+nameserv_addr=[192.168.$var_routes.1,8.8.8.8]
+
+if [ $dhcp4 == 1 ]; then
+	dhcp4_ref=true
+	echo_f
+	dhcp4_f
+else
+	dhcp4_ref=no
+	echo_f
+	dhcp4_f
+	dhcp4_stat
+fi
