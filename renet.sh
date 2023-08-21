f767c21a2a3174f8cda9ace12280582e094e4a67
Rustam Uzairov <mr.uzairov@gmail.com>
2023-08-21 20:29:06 +0300

    ...

--- renet.sh
+++ renet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 THIS_REPO_PATH="$(dirname "$(realpath "$0")")"
-netplan_generate_config="$THIS_REPO_PATH/bin/netplan_gen.sh"
+netplan_generate_config="$THIS_REPO_PATH/netplan_generate.sh
 
 sudo -E -s "$netplan_generate_config"
