d1c390d9a7bd60d105b1d4131e21978bc365f5bb
Rustam Uzairov <svrvt.zrv@gmail.com>
2024-05-30 15:51:59 +0300

    deleted the dir executable files that I moved to ~/bin/*

--- bin/wifi_list.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for i in $(command ls /sys/class/net/ | egrep -v "^lo$"); do
-	if which iw &>/dev/null; then
-		sudo iw dev "$i" scan | grep SSID | awk '{print substr($0, index($0,$2)) }'
-	fi
-done 2>/dev/null | sort -u
